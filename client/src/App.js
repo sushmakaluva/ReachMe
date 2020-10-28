a6a37c9140b0811e858fabe213362a4a9fe7e32e
Sushma Kaluva <kaluvasushma@gmail.com>
2020-10-28 14:14:15 -0400

    added bootstrap-react

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Login from './components/Login';
 
 import "./App.css";
 
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import Form from 'react-bootstrap/Form';
 
 export default function Login() {
     return (
         <div>
-
+            <Logo />
+            
         </div>
     )
 }
