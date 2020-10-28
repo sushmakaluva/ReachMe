a91825253c8f6dd6c299ffa8eeda8f49e4ec2169
Sushma Kaluva <kaluvasushma@gmail.com>
2020-10-28 15:37:30 -0400

    added login,signup form components

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import Form from 'react-bootstrap/Form';
+import {Form} from 'react-bootstrap';
+// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import SignupModal from './Modal';
 
 export default function Login() {
     return (
         <div>
             <Logo />
-            
-        </div>
+            <Form>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                </Form.Group>
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Log In
+                </Button>
+                <Button variant="success" onClick={SignupModal} >
+                    Create new Account (Sign-Up)
+                </Button>
+            </Form>
+        </div >
     )
 }
