7c08e3e71f2df01478f8c2f7c1051641c0992db9
Sushma Kaluva <kaluvasushma@gmail.com>
2020-10-30 16:28:04 -0400

    added api for /login,/signup

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const router = express.Router();
 const path = require("path");
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3010;
 const app = express();
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(router);
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
@@ -13,12 +16,13 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Define API routes here
-app.use(routes);
+require('./routes')(router);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/users");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reachme_db");
 
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
+
