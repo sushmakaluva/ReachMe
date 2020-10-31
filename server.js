38b12e2da3856c96f16fe04aa4b157d63636af06
Keith Wan <61431938+kayywan@users.noreply.github.com>
2020-10-31 14:53:27 -0400

    adding a mongodb connection

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
-const express = require("express");
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import Pusher from 'pusher'
+
+// app config
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +11,7 @@ const app = express();
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors())
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
@@ -13,10 +19,23 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Define API routes here
-app.use(routes);
+app.get('/', (req, res) => res.status(200).send('hello world'));
+// app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/users");
+
+const connection_url = 'mongodb+srv://admin:<iFo0nsf7aKodl7uB@cluster0.w6bts.mongodb.net/reachmeDB?retryWrites=true&w=majority'
+mongoose.connect(connection_url, {
+  userCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  
+mongoose.connection.once("open", () => {
+  console.log("DB Connected");
+});
+
+process.env.MONGODB_URI || "mongodb://localhost/users";
 
 
 app.listen(PORT, () => {
