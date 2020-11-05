40428625989a82e70de08de1d5f78cf209657c36
LLenroc <nc_2@hotmail.com>
2020-11-05 03:23:01 -0500

    import css

--- client/src/pages/ProfilePage.js
+++ client/src/pages/ProfilePage.js
@@ -12,17 +12,20 @@ export default function ProfilePage() {
               <div className="profile-image">
                 <img src="" alt="" />
               </div>
+              
               <div className="profile-user-settings">
                 <h1 className="profile-user-name">Profile_</h1>
                 <button className="btn profile-edit-btn">Edit Profile</button>
                 <button className="btn profile-settings-btn" aria-label="profile settings"><i className="fas fa-cog" aria-hidden="true" /></button>
               </div>
+              
               <div className="profile-stats">
                 <ul>
                   <li><span className="profile-stat-count">200</span> posts</li>
                   <li><span className="profile-stat-count">450</span> Friends</li>
                 </ul>
               </div>
+              
               <div className="profile-bio">
                 <p><span className="profile-real-name">Profile Name</span> Lorem ipsum dolor sit, amet consectetur adipisicing elit 📷✈️🏕️</p>
               </div>
@@ -35,6 +38,7 @@ export default function ProfilePage() {
             <div className="gallery">
               <div className="gallery-item" tabIndex={0}>
                 <img src="" className="gallery-image" alt="" />
+                
                 <div className="gallery-item-info">
                   <ul>
                     <li className="gallery-item-likes"><span className="visually-hidden">Likes:</span><i className="fas fa-heart" aria-hidden="true" /> 56</li>
@@ -42,6 +46,7 @@ export default function ProfilePage() {
                   </ul>
                 </div>
               </div>
+              
               <div className="gallery-item" tabIndex={0}>
                 <img src="" className="gallery-image" alt="" />
                 <div className="gallery-item-info">
@@ -51,11 +56,13 @@ export default function ProfilePage() {
                   </ul>
                 </div>
               </div>
+              
               <div className="gallery-item" tabIndex={0}>
                 <img src="" className="gallery-image" alt="" />
                 <div className="gallery-item-type">
                   <span className="visually-hidden">Gallery</span><i className="fas fa-clone" aria-hidden="true" />
                 </div>
+                
                 <div className="gallery-item-info">
                   <ul>
                     <li className="gallery-item-likes"><span className="visually-hidden">Likes:</span><i className="fas fa-heart" aria-hidden="true" /> 42</li>
