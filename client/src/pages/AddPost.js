4ac76d579c504774a09c221dc084ee643f5e2d35
Sushma Kaluva <kaluvasushma@gmail.com>
2020-11-05 12:21:10 -0500

    added firbase logic

--- client/src/pages/AddPost.js
+++ client/src/pages/AddPost.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import {storage} from "../firebase/firebase.js";
+import { storage } from "../firebase/firebase.js";
 import NavTag from '../components/NavTag';
 import API from '../utils/API';
 import { Jumbotron, Container, Form, Button } from 'react-bootstrap';
@@ -12,48 +12,48 @@ export default function AddPost() {
         caption: ""
     })
 
-    const allInputs = {imgUrl: ''};
+    const allInputs = { imgUrl: '' };
     const [imageAsFile, setImageAsFile] = useState('');
     const [imageAsUrl, setImageAsUrl] = useState(allInputs);
 
     const handleImageAsFile = (e) => {
-      const image = e.target.files[0]
-      setImageAsFile(imageFile => (image))
-  }
-
-const handleFireBaseUpload = e => {
-  e.preventDefault()
-console.log('start of upload');
-// async magic goes here...
-if(imageAsFile === '' ) {
-      console.error(`not an image, the image file is a ${typeof(imageAsFile)}`)
+        const image = e.target.files[0]
+        setImageAsFile(imageFile => (image))
     }
-}
 
-const uploadTask = storage.ref(`/images/${imageAsFile.name}`).put(imageAsFile)
+    const handleFireBaseUpload = e => {
+        e.preventDefault()
+        console.log('start of upload');
+        // async magic goes here...
+        if (imageAsFile === '') {
+            console.error(`not an image, the image file is a ${typeof (imageAsFile)}`)
+        }
+    }
 
-//initiates the firebase side uploading 
-    // uploadTask.on('state_changed', 
-    // (snapShot) => {
-    //   //takes a snap shot of the process as it is happening
-    //   console.log(snapShot)
-    // }, (err) => {
-    //   //catches the errors
-    //   console.log(err)
-    // }, () => {
-    //   // gets the functions from storage refences the image storage in firebase by the children
-    //   // gets the download url then sets the image from firebase as the value for the imgUrl key:
-    //   storage.ref('images').child(imageAsFile.name).getDownloadURL()
-    //    .then(fireBaseUrl => {
-    //      setImageAsUrl(prevObject => ({...prevObject, imgUrl: fireBaseUrl}))
-    //    })
-    // })
+    const uploadTask = storage.ref(`/images/${imageAsFile.name}`).put(imageAsFile)
+
+    initiates the firebase side uploading
+    uploadTask.on('state_changed',
+        (snapShot) => {
+            //takes a snap shot of the process as it is happening
+            console.log(snapShot)
+        }, (err) => {
+            //catches the errors
+            console.log(err)
+        }, () => {
+            // gets the functions from storage refences the image storage in firebase by the children
+            // gets the download url then sets the image from firebase as the value for the imgUrl key:
+            storage.ref('images').child(imageAsFile.name).getDownloadURL()
+                .then(fireBaseUrl => {
+                    setImageAsUrl(prevObject => ({ ...prevObject, imgUrl: fireBaseUrl }))
+                })
+        })
 
     function handleInputChange(event) {
         const { name, value } = event.target;
         setFormObject({ ...formObject, [name]: value })
     }
-    
+
     // const imgStyle = {
     //     fontWeight: "bold",
     // }
@@ -99,7 +99,7 @@ const uploadTask = storage.ref(`/images/${imageAsFile.name}`).put(imageAsFile)
                         {emojiShow ? <Emoji /> : null}
                         <br />
                         <form onSubmit={handleFireBaseUpload}>
-                         <input onChange={handleImageAsFile} type="file" />
+                            <input onChange={handleImageAsFile} type="file" />
                         </form>
                         <Button variant="success" type="submit" >
                             Add Post
