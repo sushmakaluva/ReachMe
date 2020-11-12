a93f270df0f3db7c997b86f691baae0d6a13f090
stephanie morrison <stephmorr.to@gmail.com>
2020-11-12 13:55:01 -0500

    chatapp

--- client/src/App.js
+++ client/src/App.js
@@ -7,8 +7,6 @@ import "./App.css";
 import SignupModal from './components/Modal';
 import NoMatch from './pages/NoMatch';
 import Homepage from './pages/Homepage';
-import NavBar from './components/NavBar';
-import Posts from './Posts';
 
 function App() {
 
@@ -44,6 +42,11 @@ function App() {
       </div >
     </Router>
   );
+
+ 
+    
+  
 }
+
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,27 @@ import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App';
 
 ReactDOM.render(<App />,document.getElementById("root"));
+
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io') (http);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+       io.emit('chat message',msg)
+      });
+    });
+    
+
+    http.listen(port, function(){
+        console.log('listening on *:' + port);
+});
 
 registerServiceWorker();
