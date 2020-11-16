52854b72960611caf170792c45d609f86ca9c472
Sushma Kaluva <kaluvasushma@gmail.com>
2020-11-16 16:25:37 -0500

    added build folder

---
+++ client/build/precache-manifest.dca432b42cddaf774d780cfece5ef1a4.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05fbcd1c6deee706dc6919ecc91b5f1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e30528ec2cda1404a69",
+    "url": "/static/css/2.d94e4566.chunk.css"
+  },
+  {
+    "revision": "a88778c50c4e17414918",
+    "url": "/static/css/main.999ca369.chunk.css"
+  },
+  {
+    "revision": "8e30528ec2cda1404a69",
+    "url": "/static/js/2.4fb46161.chunk.js"
+  },
+  {
+    "revision": "88363255406a1a7779ca0f8917acf9c2",
+    "url": "/static/js/2.4fb46161.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a88778c50c4e17414918",
+    "url": "/static/js/main.049c783f.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  },
+  {
+    "revision": "b46b7f71b480e498fc5035cf501733fe",
+    "url": "/static/media/bb6.b46b7f71.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
--- server.js
+++ server.js
@@ -2,38 +2,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 const router = express.Router();
 const path = require("path");
-var session = require('express-session');
 const PORT = process.env.PORT || 3001;
 const app = express();
-var cookieParser = require('cookie-parser');
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(router);
-// app.use(cookieParser());
-
-// app.use(session({
-//   secret: 'keyboard cat',
-//   cookie: { maxAge: 60000 },
-//   name: 'reachme'
-// }))
-
-// Access the session as req.session
-// app.get('/', function (req, res, next) {
-
-//   if (req.session.views) {
-//     req.session.views++
-//     res.setHeader('Content-Type', 'text/html')
-//     res.write('<p>views: ' + req.session.views + '</p>')
-//     res.write('<p>expires in: ' + (req.session.cookie.maxAge / 1000) + 's</p>')
-//     res.end()
-//   } else {
-//     req.session.views = 1
-//     res.end('welcome to the session demo. refresh!')
-//   }
-// })
-
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
@@ -44,7 +19,14 @@ if (process.env.NODE_ENV === "production") {
 require('./routes')(router);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reachme_db");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reachme_db",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
